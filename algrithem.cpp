e9eabffc1a71043c077b69a51d370bd44ab28975
barney-jin <peiyuan3@illinois.edu>
2020-11-30 10:59:43 -0600

    have bad alloc

--- algrithem.cpp
+++ algrithem.cpp
@@ -6,12 +6,15 @@
 #include <vector>
 #include <queue>
 
+
 std::vector<Vertex> BFS_Traversal(Graph& g_, Vertex rand_Vertex) {
+    
     std::vector<Vertex> toReturn;
     
     std::vector<Vertex> visited;
     std::queue<Vertex> queue_;
-    for (auto s : g_.getVertices()) {
+    vector<Vertex> all_vertex = g_.getVertices();
+    for (Vertex& s : all_vertex) {
         if (!s.visited()) {
             queue_.push(s);
         } else {
@@ -26,7 +29,6 @@ std::vector<Vertex> BFS_Traversal(Graph& g_, Vertex rand_Vertex) {
         queue_.pop();
         temp_.visit();
         toReturn.push_back(temp_);
-        
         for (auto n : neighbors) {
             if (!n.visited()) {
                 queue_.push(n);
