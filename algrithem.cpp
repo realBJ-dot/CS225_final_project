85b8a245f84e4b0af039114e14140ab7707985c2
Xinshuo Lei <xinshuo3@illinois.edu>
2020-12-02 21:37:00 -0600

    finish BFS

--- algrithem.cpp
+++ algrithem.cpp
@@ -5,30 +5,28 @@
 #include "edge.h"
 #include <vector>
 #include <queue>
+#include <map>
 
 
 std::vector<Vertex> BFS_Traversal(Graph& g_, Vertex start_vertex) {
-    std::cout << "new test" << std::endl;
     std::vector<Vertex> toReturn;
     std::queue<Vertex> queue_;
     queue_.push(start_vertex);
-    start_vertex.visit();
-    std::cout << g_.getVertices()[2].getId() << " visited: " << g_.getVertices()[3].visited() << std::endl;
+    std::map<string, bool> visited_;
+    for (auto& v : g_.getVertices()) {
+        visited_[v.getId()] = false;
+    }
+    visited_[start_vertex.getId()] = true;
     while (!queue_.empty()) {
         auto temp_ = queue_.front();
         queue_.pop();
         toReturn.push_back(temp_);
         for (Vertex& n : g_.getAdjacent(temp_)) {
-            std::cout << "neighbor of " << temp_.getId() << ": " << n.getId() << std::endl;
-            std::cout << g_.getVertices()[2].getId() << " visited: " << g_.getVertices()[3].visited() << std::endl;
-            if (!n.visited()) {
-                std::cout << "visit " << n.getId() << std::endl;
+            if (!visited_[n.getId()]) {
                 queue_.push(n);
-                n.visit();
+                visited_[n.getId()] = true;
             }
         }
     }
-    std::cout << g_.getVertices()[4].getId() << " visited: " << g_.getVertices()[4].visited() << std::endl;
-    std::cout << "node: " << toReturn.size() << std::endl;
     return toReturn;
 }
