d32670bab3adfa3d2723ed45bca1b57c3356fd38
Haoyuliu-ooyu <haoyul4@illinois.edu>
2020-12-03 21:39:51 -0600

    DFS algorithm & DFS test cases update & memory leak fixed

--- algrithem.cpp
+++ algrithem.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <queue>
 #include <map>
+#include <stack>
 
 
 std::vector<Vertex> BFS_Traversal(Graph& g_, Vertex start_vertex) {
@@ -31,5 +32,24 @@ std::vector<Vertex> BFS_Traversal(Graph& g_, Vertex start_vertex) {
     return toReturn;
 }
 std::vector<Vertex> DFS_Traversal(Graph& g_, Vertex start_vertex) {
-    return vector<Vertex>();
+    std::vector<Vertex> toReturn;
+    std::stack<Vertex> queue_;
+    queue_.push(start_vertex);
+    std::map<string, bool> visited_;
+    for (auto& v : g_.getVertices()) {
+        visited_[v.getId()] = false;
+    }
+    visited_[start_vertex.getId()] = true;
+    while (!queue_.empty()) {
+        auto temp_ = queue_.top();
+        queue_.pop();
+        toReturn.push_back(temp_);
+        for (Vertex& n : g_.getAdjacent(temp_)) {
+            if (!visited_[n.getId()]) {
+                queue_.push(n);
+                visited_[n.getId()] = true;
+            }
+        }
+    }
+    return toReturn;
 }
--- tests/test1.cpp
+++ tests/test1.cpp
@@ -26,7 +26,6 @@ vector<Graph*> getTestGraph() {
     g1 -> insertEdge(g1v1, g1v2);
     g1 -> insertEdge(g1v2, g1v3);
     g.push_back(g1);
-    std::cout << "num of vertexs in test: " << g1 -> getVertices().size() << std::endl;
 
     //g2 is a simple graph with several vertices and directed edges. expected 0 1 5 4 2 3
     Vertex g2v0 = Vertex(to_string(0));
@@ -87,6 +86,12 @@ TEST_CASE("BFS: simple graph with three nodes, expected 0 1 2") {
     REQUIRE ( v1[0].getId() == to_string(0) );
     REQUIRE ( v1[1].getId() == to_string(1) );
     REQUIRE ( v1[2].getId() == to_string(2) );
+    delete g[0];
+    g[0] = NULL;
+    delete g[1];
+    g[1] = NULL;
+    delete g[2];
+    g[2] = NULL;
 }
 
 TEST_CASE("BFS: graph with six nodes, expected 0 1 5 4 2 3") {
@@ -98,6 +103,12 @@ TEST_CASE("BFS: graph with six nodes, expected 0 1 5 4 2 3") {
     REQUIRE ( v1[3].getId() == to_string(4) );
     REQUIRE ( v1[4].getId() == to_string(2) );
     REQUIRE ( v1[5].getId() == to_string(3) );
+    delete g[0];
+    g[0] = NULL;
+    delete g[1];
+    g[1] = NULL;
+    delete g[2];
+    g[2] = NULL;
 }
 
 TEST_CASE("BFS: graph with 8 nodes, expected 0 3 2 6 4 1 7 5") {
@@ -111,6 +122,12 @@ TEST_CASE("BFS: graph with 8 nodes, expected 0 3 2 6 4 1 7 5") {
     REQUIRE ( v1[5].getId() == to_string(1) );
     REQUIRE ( v1[6].getId() == to_string(7) );
     REQUIRE ( v1[7].getId() == to_string(5) );
+    delete g[0];
+    g[0] = NULL;
+    delete g[1];
+    g[1] = NULL;
+    delete g[2];
+    g[2] = NULL;
 }
 
 //////////////////////DFS////////////////////
@@ -120,6 +137,12 @@ TEST_CASE("DFS:simple graph with three nodes, expected 0 1 2") {
     REQUIRE ( v1[0].getId() == to_string(0) );
     REQUIRE ( v1[1].getId() == to_string(1) );
     REQUIRE ( v1[2].getId() == to_string(2) );
+    delete g[0];
+    g[0] = NULL;
+    delete g[1];
+    g[1] = NULL;
+    delete g[2];
+    g[2] = NULL;
 }
 
 TEST_CASE("DFS:graph with six nodes, expected 0 1 2 4 3 5") {
@@ -131,19 +154,31 @@ TEST_CASE("DFS:graph with six nodes, expected 0 1 2 4 3 5") {
     REQUIRE ( v1[3].getId() == to_string(4) );
     REQUIRE ( v1[4].getId() == to_string(3) );
     REQUIRE ( v1[5].getId() == to_string(5) );
+    delete g[0];
+    g[0] = NULL;
+    delete g[1];
+    g[1] = NULL;
+    delete g[2];
+    g[2] = NULL;
 }
 
-TEST_CASE("DFS:graph with 8 nodes, expected 0 2 1 5 4 3 6 7") {
+TEST_CASE("DFS:graph with 8 nodes, expected 0 2 1 3 4 5 6 7") {
     vector<Graph*> g = getTestGraph();
     vector<Vertex> v1 = DFS_Traversal(*g[2], Vertex(to_string(0)));
     REQUIRE ( v1[0].getId() == to_string(0) );
     REQUIRE ( v1[1].getId() == to_string(2) );
     REQUIRE ( v1[2].getId() == to_string(1) );
-    REQUIRE ( v1[3].getId() == to_string(5) );
+    REQUIRE ( v1[3].getId() == to_string(3) );
     REQUIRE ( v1[4].getId() == to_string(4) );
-    REQUIRE ( v1[5].getId() == to_string(3) );
+    REQUIRE ( v1[5].getId() == to_string(5) );
     REQUIRE ( v1[6].getId() == to_string(6) );
     REQUIRE ( v1[7].getId() == to_string(7) );
+    delete g[0];
+    g[0] = NULL;
+    delete g[1];
+    g[1] = NULL;
+    delete g[2];
+    g[2] = NULL;
 }
 
 
