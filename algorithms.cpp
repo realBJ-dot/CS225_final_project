0cd918e8927869cf67b5498c680ea9835521d78e
Xinshuo Lei <xinshuo3@illinois.edu>
2020-12-05 15:05:35 -0600

    modify traversal to support graphs with more than 1 connected components

--- algorithms.cpp
+++ algorithms.cpp
@@ -8,22 +8,19 @@
 #include <map>
 #include <stack>
 
+using std::vector;
+
 /**
- * BFS traversal
+ * helper function for BFS traversal
  * @param g_: graph to traverse
  * @param start_vertex: starting vertex of traversal
- * @return vector that represents order of nodes visited
+ * @param toReturn: vector that represents orders of nodes visited
+ * @param visited_: a map that keeps track of whether a node is visited
  */
-std::vector<Vertex> BFS_Traversal(Graph& g_, Vertex start_vertex) {
-    std::vector<Vertex> toReturn;
+void BFS_Traversal(Graph& g_, Vertex start_vertex, vector<Vertex>& toReturn, 
+                    std::map<string, bool>& visited_) {
     std::queue<Vertex> queue_;
     queue_.push(start_vertex);
-    // map that maps id of Vertex to whether it is visited
-    // initial value for all Vertexs are false
-    std::map<string, bool> visited_;
-    for (auto& v : g_.getVertices()) {
-        visited_[v.getId()] = false;
-    }
     visited_[start_vertex.getId()] = true;
     while (!queue_.empty()) {
         auto temp_ = queue_.front();
@@ -36,25 +33,41 @@ std::vector<Vertex> BFS_Traversal(Graph& g_, Vertex start_vertex) {
             }
         }
     }
-    return toReturn;
 }
 
 /**
- * DFS traversal
+ * BFS traversal
+ * covers all connected components of the graph
  * @param g_: graph to traverse
- * @param start_vertex: starting vertex of traversal
- * @return vector that represents order of nodes visited
+ * @return vector that represents orders of nodes visited
  */
-std::vector<Vertex> DFS_Traversal(Graph& g_, Vertex start_vertex) {
-    std::vector<Vertex> toReturn;
-    std::stack<Vertex> stack_;
-    stack_.push(start_vertex);
+vector<Vertex> BFS(Graph& g_) {
+    vector<Vertex> toReturn;
     // map that maps id of Vertex to whether it is visited
     // initial value for all Vertexs are false
     std::map<string, bool> visited_;
     for (auto& v : g_.getVertices()) {
         visited_[v.getId()] = false;
     }
+    for (auto& v1: g_.getVertices()) {
+        if (!visited_[v1.getId()]) {
+            BFS_Traversal(g_, v1, toReturn, visited_);
+        }
+    }
+    return toReturn; 
+}
+
+/**
+ * helper function for BFS traversal
+ * @param g_: graph to traverse
+ * @param start_vertex: starting vertex of traversal
+ * @param toReturn: vector that represents orders of nodes visited
+ * @param visited_: a map that keeps track of whether a node is visited
+ */
+void DFS_Traversal(Graph& g_, Vertex start_vertex, vector<Vertex>& toReturn, 
+                    std::map<string, bool>& visited_) {
+    std::stack<Vertex> stack_;
+    stack_.push(start_vertex);
     visited_[start_vertex.getId()] = true;
     while (!stack_.empty()) {
         auto temp_ = stack_.top();
@@ -67,5 +80,29 @@ std::vector<Vertex> DFS_Traversal(Graph& g_, Vertex start_vertex) {
             }
         }
     }
+}
+
+/**
+ * DFS traversal
+ * covers all connected components of the graph
+ * @param g_: graph to traverse
+ * @return vector that represents orders of nodes visited
+ */
+vector<Vertex> DFS(Graph& g_) {
+    vector<Vertex> toReturn;
+    // map that maps id of Vertex to whether it is visited
+    // initial value for all Vertices are false
+    std::map<string, bool> visited_;
+    for (auto& v : g_.getVertices()) {
+        visited_[v.getId()] = false;
+    }
+    for (auto& v1: g_.getVertices()) {
+        if (!visited_[v1.getId()]) {
+            DFS_Traversal(g_, v1, toReturn, visited_);
+        }
+    } 
     return toReturn;
 }
+
+
+
--- tests/test1.cpp
+++ tests/test1.cpp
@@ -16,66 +16,101 @@ vector<Graph*> getTestGraph() {
     Graph* g1 = new Graph(false, true);
     Graph* g2 = new Graph(false, true);
     Graph* g3 = new Graph(false, true);
+    Graph* g4 = new Graph(false, true);
+    Graph* g5 = new Graph(false, true);
+    Graph* g6 = new Graph(false, true);
+
+    Vertex v0 = Vertex("0");
+    Vertex v1 = Vertex("1");
+    Vertex v2 = Vertex("2");
+    Vertex v3 = Vertex("3");
+    Vertex v4 = Vertex("4");
+    Vertex v5 = Vertex("5");
+    Vertex v6 = Vertex("6");
+    Vertex v7 = Vertex("7");
     
-    // g1 is a graph with three vertices. This is to test the simple functionality of the algorithm.
-    Vertex g1v1 = Vertex(to_string(0));
-    Vertex g1v2 = Vertex(to_string(1));
-    Vertex g1v3 = Vertex(to_string(2));
-    g1 -> insertVertex(g1v1);
-    g1 -> insertVertex(g1v2);
-    g1 -> insertVertex(g1v3);
-    g1 -> insertEdge(g1v1, g1v2);
-    g1 -> insertEdge(g1v2, g1v3);
+    // g1 is a graph with 3 vertices. This is to test the simple functionality of the algorithm.
+    // 0 --> 1 --> 2
+    g1 -> insertVertex(v2);
+    g1 -> insertVertex(v1);
+    g1 -> insertVertex(v0);
+    g1 -> insertEdge(v0, v1);
+    g1 -> insertEdge(v1, v2);
     g.push_back(g1);
 
-    // g2 is a simple graph with 6 vertices and directed edges. 
-    Vertex g2v0 = Vertex(to_string(0));
-    Vertex g2v1 = Vertex(to_string(1));
-    Vertex g2v2 = Vertex(to_string(2));
-    Vertex g2v3 = Vertex(to_string(3));
-    Vertex g2v4 = Vertex(to_string(4));
-    Vertex g2v5 = Vertex(to_string(5));
-    g2 -> insertVertex(g2v0);
-    g2 -> insertVertex(g2v1);
-    g2 -> insertVertex(g2v2);
-    g2 -> insertVertex(g2v3);
-    g2 -> insertVertex(g2v4);
-    g2 -> insertVertex(g2v5);
-    g2 -> insertEdge(g2v0, g2v1);
-    g2 -> insertEdge(g2v1, g2v2);
-    g2 -> insertEdge(g2v1, g2v4);
-    g2 -> insertEdge(g2v1, g2v5);
-    g2 -> insertEdge(g2v2, g2v4);
-    g2 -> insertEdge(g2v5, g2v4);
-    g2 -> insertEdge(g2v4, g2v3);
+    // g2 is a simple graph with 6 vertices and directed edges.
+    g2 -> insertVertex(v5);
+    g2 -> insertVertex(v4);
+    g2 -> insertVertex(v3);
+    g2 -> insertVertex(v2);
+    g2 -> insertVertex(v1);
+    g2 -> insertVertex(v0);
+    g2 -> insertEdge(v0, v1);
+    g2 -> insertEdge(v1, v2);
+    g2 -> insertEdge(v1, v4);
+    g2 -> insertEdge(v1, v5);
+    g2 -> insertEdge(v2, v4);
+    g2 -> insertEdge(v5, v4);
+    g2 -> insertEdge(v4, v3);
     g.push_back(g2);
 
     // g3 is a simple graph with 8 vertices and directed edges.
-    Vertex g3v0 = Vertex(to_string(0));
-    Vertex g3v1 = Vertex(to_string(1));
-    Vertex g3v2 = Vertex(to_string(2));
-    Vertex g3v3 = Vertex(to_string(3));
-    Vertex g3v4 = Vertex(to_string(4));
-    Vertex g3v5 = Vertex(to_string(5));
-    Vertex g3v6 = Vertex(to_string(6));
-    Vertex g3v7 = Vertex(to_string(7));
-    g3 -> insertVertex(g3v0);
-    g3 -> insertVertex(g3v1);
-    g3 -> insertVertex(g3v2);
-    g3 -> insertVertex(g3v3);
-    g3 -> insertVertex(g3v4);
-    g3 -> insertVertex(g3v5);
-    g3 -> insertVertex(g3v6);
-    g3 -> insertVertex(g3v7);
-    g3 -> insertEdge(g3v0, g3v2);
-    g3 -> insertEdge(g3v0, g3v3);
-    g3 -> insertEdge(g3v2, g3v1);
-    g3 -> insertEdge(g3v3, g3v4);
-    g3 -> insertEdge(g3v4, g3v5);
-    g3 -> insertEdge(g3v5, g3v1);
-    g3 -> insertEdge(g3v3, g3v6);
-    g3 -> insertEdge(g3v6, g3v7);
+    g3 -> insertVertex(v7);
+    g3 -> insertVertex(v6);    
+    g3 -> insertVertex(v5);
+    g3 -> insertVertex(v4);
+    g3 -> insertVertex(v3);
+    g3 -> insertVertex(v2);
+    g3 -> insertVertex(v1);
+    g3 -> insertVertex(v0);
+    g3 -> insertEdge(v0, v2);
+    g3 -> insertEdge(v0, v3);
+    g3 -> insertEdge(v2, v1);
+    g3 -> insertEdge(v3, v4);
+    g3 -> insertEdge(v4, v5);
+    g3 -> insertEdge(v5, v1);
+    g3 -> insertEdge(v3, v6);
+    g3 -> insertEdge(v6, v7);
     g.push_back(g3);
+
+//////// test traversal on graph with more than 1 connected components //////
+
+    // g4 is a graph with 3 vertices, all disconnected
+    g4 -> insertVertex(v2);
+    g4 -> insertVertex(v1);
+    g4 -> insertVertex(v0);
+    g.push_back(g4);
+
+    // g5 is a graph with 6 vertices, directed edges, and 2 connected components.
+    g5 -> insertVertex(v5);
+    g5 -> insertVertex(v4);
+    g5 -> insertVertex(v3);
+    g5 -> insertVertex(v2);
+    g5 -> insertVertex(v1);
+    g5 -> insertVertex(v0);
+    g5 -> insertEdge(v0, v3);
+    g5 -> insertEdge(v1, v2);
+    g5 -> insertEdge(v2, v4);
+    g5 -> insertEdge(v2, v5);
+    g5 -> insertEdge(v4, v1);
+    g.push_back(g5);
+
+    // g6 is a simple graph with 8 vertices, directed edges, and 4 connected components.
+    g6 -> insertVertex(v7);
+    g6 -> insertVertex(v6);    
+    g6 -> insertVertex(v5);
+    g6 -> insertVertex(v4);
+    g6 -> insertVertex(v3);
+    g6 -> insertVertex(v2);
+    g6 -> insertVertex(v1);
+    g6 -> insertVertex(v0);
+    g6 -> insertEdge(v4, v1);
+    g6 -> insertEdge(v1, v5);
+    g6 -> insertEdge(v5, v4);
+    g6 -> insertEdge(v2, v7);
+    g6 -> insertEdge(v2, v6);
+    g6 -> insertEdge(v6, v7);
+    g.push_back(g6);
 
     return g;
 }
@@ -88,79 +123,157 @@ void clear(vector<Graph*> g) {
 }
 
 
-TEST_CASE("BFS: simple graph with three nodes, expected 0 1 2") {
+TEST_CASE("BFS: simple graph with 3 nodes, expected 0 1 2") {
     vector<Graph*> g = getTestGraph();
-    vector<Vertex> v1 = BFS_Traversal(*g[0], Vertex(to_string(0)));
-    REQUIRE ( v1[0].getId() == to_string(0) );
-    REQUIRE ( v1[1].getId() == to_string(1) );
-    REQUIRE ( v1[2].getId() == to_string(2) );
+    vector<Vertex> v1 = BFS(*g[0]);
+    REQUIRE ( v1.size() == 3 ) ;
+    REQUIRE ( v1[0].getId() == "0" );
+    REQUIRE ( v1[1].getId() == "1" );
+    REQUIRE ( v1[2].getId() == "2" );
     clear(g);
 }
 
-TEST_CASE("BFS: graph with six nodes, expected 0 1 5 4 2 3") {
+TEST_CASE("BFS: graph with 6 nodes, expected 0 1 5 4 2 3") {
     vector<Graph*> g = getTestGraph();
-    vector<Vertex> v1 = BFS_Traversal(*g[1], Vertex(to_string(0)));
-    REQUIRE ( v1[0].getId() == to_string(0) );
-    REQUIRE ( v1[1].getId() == to_string(1) );
-    REQUIRE ( v1[2].getId() == to_string(5) );
-    REQUIRE ( v1[3].getId() == to_string(4) );
-    REQUIRE ( v1[4].getId() == to_string(2) );
-    REQUIRE ( v1[5].getId() == to_string(3) );
+    vector<Vertex> v1 = BFS(*g[1]);
+    REQUIRE ( v1.size() == 6 ) ;
+    REQUIRE ( v1[0].getId() == "0" );
+    REQUIRE ( v1[1].getId() == "1" );
+    REQUIRE ( v1[2].getId() == "5" );
+    REQUIRE ( v1[3].getId() == "4" );
+    REQUIRE ( v1[4].getId() == "2" );
+    REQUIRE ( v1[5].getId() == "3" );
     clear(g);
 }
 
 TEST_CASE("BFS: graph with 8 nodes, expected 0 3 2 6 4 1 7 5") {
     vector<Graph*> g = getTestGraph();
-    vector<Vertex> v1 = BFS_Traversal(*g[2], Vertex(to_string(0)));
-    REQUIRE ( v1[0].getId() == to_string(0) );
-    REQUIRE ( v1[1].getId() == to_string(3) );
-    REQUIRE ( v1[2].getId() == to_string(2) );
-    REQUIRE ( v1[3].getId() == to_string(6) );
-    REQUIRE ( v1[4].getId() == to_string(4) );
-    REQUIRE ( v1[5].getId() == to_string(1) );
-    REQUIRE ( v1[6].getId() == to_string(7) );
-    REQUIRE ( v1[7].getId() == to_string(5) );
+    vector<Vertex> v1 = BFS(*g[2]);
+    REQUIRE ( v1.size() == 8 ) ;
+    REQUIRE ( v1[0].getId() == "0" );
+    REQUIRE ( v1[1].getId() == "3" );
+    REQUIRE ( v1[2].getId() == "2" );
+    REQUIRE ( v1[3].getId() == "6" );
+    REQUIRE ( v1[4].getId() == "4" );
+    REQUIRE ( v1[5].getId() == "1" );
+    REQUIRE ( v1[6].getId() == "7" );
+    REQUIRE ( v1[7].getId() == "5" );
     clear(g);
 }
+
+TEST_CASE("BFS: graph with 3 disconnected nodes, expected 0 1 2") {
+    vector<Graph*> g = getTestGraph();
+    vector<Vertex> v1 = BFS(*g[3]);
+    REQUIRE ( v1.size() == 3 ) ;
+    REQUIRE ( v1[0].getId() == "0" );
+    REQUIRE ( v1[1].getId() == "1" );
+    REQUIRE ( v1[2].getId() == "2" );
+    clear(g);
+}
+
+TEST_CASE("BFS: graph with 6 nodes and 2 connected components, expected 0 3 1 2 5 4") {
+    vector<Graph*> g = getTestGraph();
+    vector<Vertex> v1 = BFS(*g[4]);
+    REQUIRE ( v1.size() == 6 ) ;
+    REQUIRE ( v1[0].getId() == "0" );
+    REQUIRE ( v1[1].getId() == "3" );
+    REQUIRE ( v1[2].getId() == "1" );
+    REQUIRE ( v1[3].getId() == "2" );
+    REQUIRE ( v1[4].getId() == "5" );
+    REQUIRE ( v1[5].getId() == "4" );
+    clear(g);
+}
+
+TEST_CASE("BFS: graph with 8 nodes and 4 connected components,  expected 0 1 5 4 2 6 7 3") {
+    vector<Graph*> g = getTestGraph();
+    vector<Vertex> v1 = BFS(*g[5]);
+    REQUIRE ( v1.size() == 8 ) ;
+    REQUIRE ( v1[0].getId() == "0" );
+    REQUIRE ( v1[1].getId() == "1" );
+    REQUIRE ( v1[2].getId() == "5" );
+    REQUIRE ( v1[3].getId() == "4" );
+    REQUIRE ( v1[4].getId() == "2" );
+    REQUIRE ( v1[5].getId() == "6" );
+    REQUIRE ( v1[6].getId() == "7" );
+    REQUIRE ( v1[7].getId() == "3" );
+    clear(g);
+}
+
 
 //////////////////////DFS test cases////////////////////
 TEST_CASE("DFS:simple graph with three nodes, expected 0 1 2") {
     vector<Graph*> g = getTestGraph();
-    vector<Vertex> v1 = DFS_Traversal(*g[0], Vertex(to_string(0)));
-    REQUIRE ( v1[0].getId() == to_string(0) );
-    REQUIRE ( v1[1].getId() == to_string(1) );
-    REQUIRE ( v1[2].getId() == to_string(2) );
-    delete g[0];
-    g[0] = NULL;
-    delete g[1];
-    g[1] = NULL;
-    delete g[2];
-    g[2] = NULL;
+    vector<Vertex> v1 = DFS(*g[0]);
+    REQUIRE ( v1.size() == 3 ) ;
+    REQUIRE ( v1[0].getId() == "0" );
+    REQUIRE ( v1[1].getId() == "1" );
+    REQUIRE ( v1[2].getId() == "2" );
+    clear(g);
 }
 
 TEST_CASE("DFS:graph with six nodes, expected 0 1 2 4 3 5") {
     vector<Graph*> g = getTestGraph();
-    vector<Vertex> v1 = DFS_Traversal(*g[1], Vertex(to_string(0)));
-    REQUIRE ( v1[0].getId() == to_string(0) );
-    REQUIRE ( v1[1].getId() == to_string(1) );
-    REQUIRE ( v1[2].getId() == to_string(2) );
-    REQUIRE ( v1[3].getId() == to_string(4) );
-    REQUIRE ( v1[4].getId() == to_string(3) );
-    REQUIRE ( v1[5].getId() == to_string(5) );
+    vector<Vertex> v1 = DFS(*g[1]);
+    REQUIRE ( v1.size() == 6 ) ;
+    REQUIRE ( v1[0].getId() == "0" );
+    REQUIRE ( v1[1].getId() == "1" );
+    REQUIRE ( v1[2].getId() == "2" );
+    REQUIRE ( v1[3].getId() == "4" );
+    REQUIRE ( v1[4].getId() == "3" );
+    REQUIRE ( v1[5].getId() == "5" );
     clear(g);
 }
 
 TEST_CASE("DFS:graph with 8 nodes, expected 0 2 1 3 4 5 6 7") {
     vector<Graph*> g = getTestGraph();
-    vector<Vertex> v1 = DFS_Traversal(*g[2], Vertex(to_string(0)));
-    REQUIRE ( v1[0].getId() == to_string(0) );
-    REQUIRE ( v1[1].getId() == to_string(2) );
-    REQUIRE ( v1[2].getId() == to_string(1) );
-    REQUIRE ( v1[3].getId() == to_string(3) );
-    REQUIRE ( v1[4].getId() == to_string(4) );
-    REQUIRE ( v1[5].getId() == to_string(5) );
-    REQUIRE ( v1[6].getId() == to_string(6) );
-    REQUIRE ( v1[7].getId() == to_string(7) );
+    vector<Vertex> v1 = DFS(*g[2]);
+    REQUIRE ( v1.size() == 8 ) ;
+    REQUIRE ( v1[0].getId() == "0" );
+    REQUIRE ( v1[1].getId() == "2" );
+    REQUIRE ( v1[2].getId() == "1" );
+    REQUIRE ( v1[3].getId() == "3" );
+    REQUIRE ( v1[4].getId() == "4" );
+    REQUIRE ( v1[5].getId() == "5" );
+    REQUIRE ( v1[6].getId() == "6" );
+    REQUIRE ( v1[7].getId() == "7" );
+    clear(g);
+}
+
+TEST_CASE("DFS: graph with 3 disconnected nodes, expected 0 1 2") {
+    vector<Graph*> g = getTestGraph();
+    vector<Vertex> v1 = DFS(*g[3]);
+    REQUIRE ( v1.size() == 3 ) ;
+    REQUIRE ( v1[0].getId() == "0" );
+    REQUIRE ( v1[1].getId() == "1" );
+    REQUIRE ( v1[2].getId() == "2" );
+    clear(g);
+}
+
+TEST_CASE("DFS: graph with 6 nodes and 2 connected components, expected 0 3 1 2 4 5") {
+    vector<Graph*> g = getTestGraph();
+    vector<Vertex> v1 = DFS(*g[4]);
+    REQUIRE ( v1.size() == 6 ) ;
+    REQUIRE ( v1[0].getId() == "0" );
+    REQUIRE ( v1[1].getId() == "3" );
+    REQUIRE ( v1[2].getId() == "1" );
+    REQUIRE ( v1[3].getId() == "2" );
+    REQUIRE ( v1[4].getId() == "4" );
+    REQUIRE ( v1[5].getId() == "5" );
+    clear(g);
+}
+
+TEST_CASE("DFS: graph with 8 nodes and 4 connected components,  expected 0 1 5 4 2 7 6 3") {
+    vector<Graph*> g = getTestGraph();
+    vector<Vertex> v1 = DFS(*g[5]);
+    REQUIRE ( v1.size() == 8 ) ;
+    REQUIRE ( v1[0].getId() == "0" );
+    REQUIRE ( v1[1].getId() == "1" );
+    REQUIRE ( v1[2].getId() == "5" );
+    REQUIRE ( v1[3].getId() == "4" );
+    REQUIRE ( v1[4].getId() == "2" );
+    REQUIRE ( v1[5].getId() == "7" );
+    REQUIRE ( v1[6].getId() == "6" );
+    REQUIRE ( v1[7].getId() == "3" );
     clear(g);
 }
 
